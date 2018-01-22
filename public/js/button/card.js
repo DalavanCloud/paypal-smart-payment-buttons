335f9ae801ba0e9f59aa23bd073bc650f39d1242
Hoang Trieu, Khang <khoangtrieu@paypal.com>
2018-01-21 19:23:15 -0800

    chore/resize zombo client

--- public/js/button/card.js
+++ public/js/button/card.js
@@ -124,4 +124,10 @@ export function renderCardExperience(props = {}) {
     // animate zombo
     const zomboEl = document.getElementById('cardExp');
     zomboEl.className = 'cardExpOpened';
+
+    // resize the button iframe for the animation
+    // TODO: make the size dynamic
+    // width = BUTTON's width
+    // height = BUTTON's height + zombo height
+    window.xchild.resize(250, 360);
 }

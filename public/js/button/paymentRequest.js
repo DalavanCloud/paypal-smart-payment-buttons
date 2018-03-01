cc6dc9410ca79a65500660eb485014909c625ff3
Hoang Trieu, Khang <khoangtrieu@paypal.com>
2018-03-01 15:48:56 -0800

    feat/ guest/hosted field flow eligibility check

--- public/js/button/paymentRequest.js
+++ public/js/button/paymentRequest.js
@@ -37,3 +37,22 @@ const details = {
 };
 
 export const payment = new PaymentRequest(supportedInstruments, details); // eslint-disable-line no-undef
+
+export const guestEligibilityCheck = ({ token, transactionCountry = 'US' }) => {
+    const params = {
+        operation: 'GuestFlowCheck',
+        query: `query GuestFlowCheck { guestFlowCheck( transactionCountry: "US" token: "${
+            token
+        }" ) { isGuestAllowed isHostFieldAllowed }}`,
+        variables: null
+    };
+
+    return fetch('https://localhost.paypal.com:8443/graphql', // eslint-disable-line no-undef
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(params)
+        });
+};
